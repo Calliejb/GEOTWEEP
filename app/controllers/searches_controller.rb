023fde63c94a40345310f06f86c941af7fcc03e0
Callie Burke <calliejamesburke@gmail.com>
2014-06-02 18:05:23 -0700

    First commit

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,47 @@
+class SearchesController < ApplicationController
+	require 'rubygems'
+	require 'twitter'
+  def index
+  	@searches = Search.all
+
+    twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "ZVIgrKzFHV0s7MkUPsUaRxSB0"
+      config.consumer_secret     = "8ZvyhbwenIBiiDZX2V5jbfCyClvliVX08nBmKCJWs8JthZDapL"
+      config.access_token        = "30171655-TFN84aT0l0qqI5BgxCCko9Ueg2iHNCOlFPQhmBiw2"
+      config.access_token_secret = "nGsiCp8tgYZ90CbDSfDjgB4ytCnF9GqXbb40XLetkGPpi"
+    end
+
+    if params.has_key?(:term) && params[:term] != ""
+      @tweets = twitter.search(params[:term], result_type: "recent").take(500)
+      # @t1 = @tweets[0].to_h
+
+    else
+      @tweets = twitter.search("geo", result_type: "recent").take(10)
+      @t1 = @tweets[0].to_h
+    end
+
+    
+    twitter.search(:term, result_type: "recent").take(5).each do |tweet|
+      @twt = tweet.text
+    end
+
+  end
+
+  def new
+
+    @search = Search.new
+
+  end
+
+  def create
+    @search = Search.new(params.require(:search).permit(:term))
+    if @search.save
+      redirect_to searches_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweetApp::Application.config.secret_key_base = '3db64750552d0b1c387e99792277ba5e5070bc739f7beb79a95eaa98feb5ae0386eb0c68b01d64b4f1ef5465a297ceee42d3f20f451c5f6db26087b53f9a8294'
