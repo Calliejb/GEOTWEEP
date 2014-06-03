8e66757173ea441e79ad987191b2423898c841fe
tthomander@gmail.com <tthomander@gmail.com>
2014-06-02 19:55:47 -0700

    added foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require underscore
 //= require gmaps/google
+//= require foundation
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
