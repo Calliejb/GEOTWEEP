351db4ef993909d0ba8478c4c7416ebca77c0c74
Clarence Dang <cdang@Clarences-MacBook-Pro.local>
2014-06-04 18:56:32 -0700

    fixed off-canvas bar and installed factorygirl

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem 'gmaps4rails'
 
 group :development, :test do
   gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
 
 group :test do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "has a valid factory" do
+		expect(FactoryGirl.build(:user)).to be_valid
+	end
+
+	it "is invalid without an email" do
+		noemail = FactoryGirl.build(:user, email: nil)
+		expect(noemail).to be_invalid
+	end
+
+	it "is invalid without a password" do
+		passwordless = FactoryGirl.build(:user, password: nil)
+		expect(passwordless).to be_invalid
+	end
+  
+end
+
+describe "Sign-In Page" do
+	describe "with valid sign-in information" do
+		before { sign_in(:user) } 
+
+		it { should have_selector('a', text: 'Sign up')}
+        it { should have_selector('a', text: 'Sign in')}
+        it { should have_selector('a', text: 'Sign Out') }
+	end
 end
