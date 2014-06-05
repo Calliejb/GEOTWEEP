9078d64b99c01c7fd931f5a1e8fc0db77bbc6df9
Callie Burke <calliejamesburke@gmail.com>
2014-06-04 21:59:25 -0700

    Added angular and some angular functions in js file

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,4 +1,5 @@
 
+
 var geotweepApp = angular.module('GeoTweepApp', ['ngResource']).config(
 	['$httpProvider', function($httpProvider) {
 	var authToken = angular.element("meta[name=\"csrf-token\"]").attr("content");
@@ -9,3 +10,38 @@ var geotweepApp = angular.module('GeoTweepApp', ['ngResource']).config(
 	defaults.patch['Content-Type'] = 'application/json';
 	defaults.common['Accept'] = 'application/json';
 }]);
+
+geotweepApp.factory('Search', ['$resource', function($resource) {
+  return $resource('/searches/:id',
+     {id: '@id'},
+     {update: { method: 'PATCH'}});
+}]);
+
+geotweepApp.controller('GeoCtrl', ['$scope', 'Search', function($scope, Search) {
+    $scope.searches= [];
+
+    $scope.newSearch = new Search();
+
+    Search.query(function(searches) {
+      $scope.searches = searches;
+   });
+
+    $scope.saveSearch = function () {
+      $scope.newSearch.$save(function(search) {
+        $scope.searches.push(search);
+        $scope.newSearch = new Search();
+      });
+    };
+
+    $scope.showSearchField = function(search) {
+      // search.terms = true;
+    };
+
+    $scope.hideSearchField = function(search) {
+      // search.terms = false;
+    };
+
+    $scope.clearErrors = function() {
+      $scope.errors = null;
+    }
+}])
